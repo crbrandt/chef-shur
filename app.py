ce108c897876de600de57b4c269ba3bf55eb3e30
crbrandt <83561834+crbrandt@users.noreply.github.com>
2023-01-29 15:38:25 -0800

    Update app.py

--- app.py
+++ app.py
@@ -11,22 +11,18 @@
 
 
 import pandas as pd
-import numpy as np
 import streamlit as st
 
 
-from scipy import stats
 import requests
 import io
-from sklearn.metrics import accuracy_score
+
 
 import html5lib
 
 
 from datetime import date
 from datetime import datetime
-
-import math
 
 
 # In[2]:
