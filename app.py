3e7d51e7f807b22bda9c67996a192ce19b2c8e1f
crbrandt <83561834+crbrandt@users.noreply.github.com>
2023-01-29 19:53:11 -0800

    Update app.py

--- app.py
+++ app.py
@@ -65,18 +65,17 @@ rsvp = pd.read_csv(rsvp_url, error_bad_lines=False)
 auth = 'undefined' 
 
 while auth != True:
-  with st.form(key='my_form'):
-	guest_id = st.text_input(label="guest id", key='guest_id')
-	password = st.text_input(label="password", key='password')
-	submit_button = st.form_submit_button(label='submit')
-
-  
-  if guest_id == 'test' and password == 'test':
-    auth = True
-  elif guest_id == 'test1' and password == 'test1':
-    auth = True
-  else:
-    st.write('Invalid.')
+	with st.form(key='my_form'):
+		guest_id = st.text_input(label="guest id", key='guest_id')
+		password = st.text_input(label="password", key='password')
+		submit_button = st.form_submit_button(label='submit')
+		
+	if guest_id == 'test' and password == 'test':
+		auth = True
+	elif guest_id == 'test1' and password == 'test1':
+		auth = True
+	else:
+		st.write('Invalid.')
  
 
 while auth == True:
