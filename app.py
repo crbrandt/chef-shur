277c7333a468f566b16dccfe5c0d9ad6492fc220
crbrandt <83561834+crbrandt@users.noreply.github.com>
2023-01-29 18:31:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ season_start = datetime.strptime('2021-09-07', '%Y-%m-%d').date()
 #about = st.beta_expander('About')
 with about:
     st.image("https://i.ibb.co/hDRKYT4/png-transparent-chef-s-uniform-hat-toque-hat-removebg-preview.jpg",
-    #width= 100, caption='2023 Cole Brandt')
+    width= 100, caption='2023 Cole Brandt')
     
 
 
